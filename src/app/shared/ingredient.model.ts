fe5716d7ef0a8b2bfda1cbacae819ef074184d0b
akshay <pandeya@vmcmail.com>
2017-08-23 11:33:38 -0500

    basic shopping list added

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,11 +1,7 @@
 export class Ingredient{
 
-    public name:string;
-    public amount: number;
+    constructor( public name:string, public amount: number ){
 
-    constructor( name:string, amount: number ){
-
-        this.name = name;
-        this.amount = amount;
+    
     }
 }
